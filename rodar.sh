707f0483c14ff9dd75ba0566d9ade2221cc43af7
luiza-diapp <luizawdiapp@gmail.com>
2025-11-08 11:47:49 -0300

    alteracoes na funcao xalign e na rodar.sh

--- rodar.sh
+++ rodar.sh
@@ -3,4 +3,10 @@
 # Este script executa o programa knn_mpi com os parâmetros especificados.
 
 # O comando completo:
-mpirun -np 2 ./knn_mpi nq=128 npp=400000 d=300 k=1024
+echo "Primeiro experimento np=1"
+mpirun -np 1 ./knn_mpi nq=128 npp=400000 d=300 k=1024
+
+echo "Segundo experimento np=8"
+mpirun -np 8 ./knn_mpi nq=128 npp=400000 d=300 k=1024
+
+echo "Terceiro experimento"
